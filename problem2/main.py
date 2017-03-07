6a04b6efbdf900c5b825dcdc21cc4b339fc26c50
Babich Mikhail <Babich-MV@yandex.ru>
2017-03-07 16:24:41 +1000

    Add comments for report

--- problem2/main.py
+++ problem2/main.py
@@ -1,10 +1,18 @@
 import time as time
 
+# It's class Vertex for one Vertex in graph. Graph is array of vertex
+# attributes: used, edges
+# edges - nubmer of vertexes (indexes elements in array of Vertex)
+# used - bool , using in dfs for mark vertex
 class Vertex:
     def __init__(self):
         self.edges = []
         self.used = False
 
+
+# It's DFS
+# input args: graph (array of Vertex), v (int - current vertex), dest (int - destination)
+# output args: path (array of int - reverse path from v to dest)
 def dfs(graph, v, dest):
     graph[v].used = True
     if v == dest:
@@ -17,6 +25,9 @@ def dfs(graph, v, dest):
                 return path
     return []
 
+# It's function for initialization graph. Graph represented as lists
+# input args: lines (array of string from test file)
+# output args: graph (array of Vertex)
 def graph_init(lines, n, m):
     graph = [ Vertex() for i in range(n) ]
     for i in range(1, m + 1):
@@ -27,9 +38,11 @@ def graph_init(lines, n, m):
         graph[v].edges.append(u)
     return graph
 
+# It's function for check execution time and run dfs
+# input args: graph (array of Vertex), vertexA, vertexB (int)
+# output args: path: array of int (number of vertexes)
 def solve(graph, a, b):
     begin = time.clock()
     path = dfs(graph, a, b)
-    print("time: %f", begin - time.clock())
+    print("time: ", begin - time.clock())
     return path
-    
--- problem3/main.py
+++ problem3/main.py
@@ -1,16 +1,25 @@
 import time as time
 
+# It's class Vertex for one Vertex in graph. Graph is array of vertex
+# attributes: used, edges
+# edges - nubmer of vertexes (indexes of elements in array of Vertex)
+# used - bool , using in dfs for mark vertex
 class Vertex:
     def __init__(self):
         self.edges = []
         self.used = False
 
+# It's sample dfs. Only marked vertex
+# input args: graph (array of Vertex), v (int - current vertex)
 def dfs(graph, v):
     graph[v].used = True
     for i in graph[v].edges:
         if not graph[i].used:
             dfs(graph, i)
 
+# It's function for initialization graph. Graph represented as lists
+# input args: lines (array of string from test file)
+# output args: graph (array of Vertex)
 def graph_init(lines, n, m):
     graph = [ Vertex() for i in range(n) ]
     for i in range(1, m + 1):
@@ -24,6 +33,9 @@ def graph_init(lines, n, m):
 def solve(graph):
     begin = time.clock()
     count = 0
+    # If vertex not used after dfs that it's new connected component;
+    # count - number of connected components
+    # If vertex not used than run dfs and all vertex in current connected component are used
     for i in range(len(graph)):
         if not graph[i].used:
             dfs(graph, i)
